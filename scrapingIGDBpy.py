0be5899338d6dcc95c59eebe1a404b02d79e43a5
Rena Repenning <renareps108@gmail.com>
2021-11-17 12:05:24 -0600

    update matts

--- scrapingIGDBpy.py
+++ scrapingIGDBpy.py
@@ -7,6 +7,7 @@ import pandas as pd
 from IPython.core.display import display, HTML
 import datetime as dt
 import numpy as np
+import os
 
 
 url = "https://www.igdb.com/games/forza-horizon-5"
@@ -101,6 +102,41 @@ data = list(
 # data
 
 
-for x in pd.read_html('https://en.wikipedia.org/wiki/List_of_Xbox_One_X_enhanced_games'):
-    print("PENIS")
-    print(x.columns)
+def get_urls(main_url: str = 'https://en.wikipedia.org/wiki/Lists_of_video_games'):
+
+    r = requests.get(wiki_path, headers=headers)
+    data = r.text.split('href="/wiki/List_of')
+    data = list(
+        map(lambda x: 'https://en.wikipedia.org/wiki/List_of' + x.split('" ')[0], data))
+    return data
+
+
+def get_data(urls: list = get_urls()):
+    master_path = 'Game Data'
+    if not os.path.exists(master_path):
+        os.mkdir(master_path)
+
+    for url in urls:
+        title = url.split('https://en.wikipedia.org/wiki/List_of')[1][1:]
+        out_path = os.path.join(title, title + '.csv')
+        if not os.path.exists(title):
+            os.mkdir(title)
+        try:
+            df = pd.read_html(url)
+            print(url, 'Finished')
+
+            # Uncomment this line to populate data folders
+            # df.to_csv(out_path)
+
+        except:
+            print(url, 'Invalid, no Table Muchacho')
+            pass
+
+
+for df in pd.read_html('https://en.wikipedia.org/wiki/List_of_crossovers_in_video_games'):
+    if (type(df.columns) == pd.MultiIndex):
+        df.columns = list(map(lambda x: x[1], df.columns.tolist()))
+
+
+get_data()
+os.getcwd()
