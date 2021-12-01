61a075933d2e82618a4f0ae35dee3ffdac11a135
Rena Repenning <renareps108@gmail.com>
2021-12-01 10:51:49 -0600

    Edits

--- BetaRecommender/User.py
+++ BetaRecommender/User.py
@@ -1,22 +1,22 @@
 class User:
-  def __init__(self, name, att1, att2, attN):
-    self.name = name
-    self.att1 = att1
-    self.att2 = att2
-    self.attN = attN
-    
-  def printUser(self):
-    print(user," profile:\n",att1", "att2"," attN)
-    
-    
-# get user input 
-attributes = [X, Y, Z]
+    def __init__(self, name, att1, att2, attN):
+        self.name = name
+        self.att1 = att1
+        self.att2 = att2
+        self.attN = attN
+
+    def printUser(self):
+        print(self.name, " profile:\n", self.att1", "self.att2"," self.attN)
+
+
+# get user input
+attributes = ["X", "Y", "Z"]
 choices = []
-n = input("Name: " )
+n = input("Name: ")
 
 for i in attributes:
-  choices += input("please rate the importance of ",i," from 1-10: " )
-     
+    choices += input("please rate the importance of ", i, " from 1-10: ")
+
 me = User(n, choices[0], choices[1], choices[2])
 
 me.printUser()
