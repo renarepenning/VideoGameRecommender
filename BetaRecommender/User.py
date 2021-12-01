1a02d3fdfa02821547f2e390a41d9e3671508c89
Rena Repenning <65434867+renarepenning@users.noreply.github.com>
2021-12-01 12:31:40 -0600

    Update User.py

--- BetaRecommender/User.py
+++ BetaRecommender/User.py
@@ -4,20 +4,19 @@ class User:
         self.genre = genre
         self.console = console
         self.key_words = keywords
+        
+        # get user input
+        attributes = ["List your console(s): ", "Name 1-3 genres: ", "Name 1-3 keywords: "]
+        choices = []
+        n = input("Name: ")
+        for i in attributes:
+            choices += input(i)
+        me = User(n, choices[0], choices[1], choices[2])
+        me.printUser()
 
 
     def printUser(self):
         print(self.name, " profile:\n", self.console", "self.genre"," self.key_words)
 
 
-# get user input
-attributes = ["List your console(s): ", "Name 1-3 genres: ", "Name 1-3 keywords: "]
-choices = []
-n = input("Name: ")
 
-for i in attributes:
-    choices += input(i)
-
-me = User(n, choices[0], choices[1], choices[2])
-
-me.printUser()
