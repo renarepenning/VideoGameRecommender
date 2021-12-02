26da6d3b1158d31edb01cc83b49a7766d815aaa9
Rena Repenning <renareps108@gmail.com>
2021-12-01 20:50:55 -0600

    reading files

--- BetaRecommender/User.py
+++ BetaRecommender/User.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class User:
     def __init__(self):  # self, name, genres, consoles, key_words):
         self.name = input("Name: ")
@@ -7,6 +10,9 @@ class User:
             "List your genre(s) with commas and no spaces: ").split(",")
         self.key_words = input(
             "List your key_words(s) with commas and no spaces: ").split(",")
+
+        self.series = pd.Series(data=[self.name, self.consoles, self.genres, self.key_words],
+                                index=["user", "consoles", "genres", "key_words"])
 
     def printUser(self):
         print(self.name, "'s profile:\n- ", self.consoles,
--- BetaRecommender/classificationAlg.py
+++ BetaRecommender/classificationAlg.py
@@ -1,14 +1,25 @@
-import requests
-from User import User
 from scraping_igdb import get_token
+from User import User
+import pandas as pd
+#!pip3 install openpyxl  # package to open xlsx files
+#!pip3 install xlrd
 '''implement classification alg - KNN classifier'''
+features = ["name", "game", "studio", "console", "keywords", "similar games"]
 
 # Get user name and ratings
 new_user = User()
 new_user.printUser()
 
-# pull data from indie games
-features = ["name", "game", "studio", "console", "keywords", "similar games"]
+# open excel file --> EXCEL READING NOT WORKING??
+allGames = pd.read_csv("IGDB_games_beta.csv")
+allGames.columns = ["id", "name", "category", "genres", "themes",
+                    "involved_companies", "platforms", "key_words", "similar_games", "release_dates", ]
+# allGames.reindex("")
+print(allGames)
+# .head() wont work # https://stackoverflow.com/questions/62624980/vscode-python-pandas-dataframe-intellisense-doesnt-show-attributes-method
+
+userFeature = new_user.series
+print(userFeature)
 
 
 '''# scraping IGDB
