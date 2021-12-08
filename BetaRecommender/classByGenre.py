7ea987697c2a2c1bc1a25471fc73f51a94808594
Rena Repenning <renareps108@gmail.com>
2021-12-08 11:52:22 -0600

    add files

---
+++ BetaRecommender/classByGenre.py
@@ -0,0 +1,13 @@
+# array of acceptable genres
+# Input: 10 games -->
+
+# all movies w/in these genres, w/in accepted
+# --> console as filer --> genres as filter, queried data - keywords mlo
+
+# query: {name:keywords}
+
+# data in: name, keywords
+
+# process: calculate dist as # common keywords, to keywords in games user chose
+
+# output: closest games to target keywords
