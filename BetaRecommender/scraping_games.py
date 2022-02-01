5bafdabe5dde5d0841649824f4c4d5f5eedc4fee
kromer-creator <69378589+kromer-creator@users.noreply.github.com>
2022-02-01 14:04:34 -0600

    Final Code to Scrape ALL Games.

---
+++ BetaRecommender/scraping_games.py
@@ -0,0 +1,35 @@
+# Kelly Romer
+
+import requests
+from igdb.wrapper import IGDBWrapper
+import json
+
+# Get Access Token
+def get_token():
+    client_id = '8kx354hpu7lzs6ga7c1ktafopq30qq'
+    client_secret = 'yucgxc2bqiobcn2pfadu5bui5amuan'
+    body = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        "grant_type": 'client_credentials'
+    }
+    r = requests.post('https://id.twitch.tv/oauth2/token', body)
+    keys = r.json()
+    return keys['access_token']
+
+access_token = get_token()
+
+# Create Wrapper
+wrapper = IGDBWrapper('8kx354hpu7lzs6ga7c1ktafopq30qq', access_token)
+
+# JSON API request
+json_arr = []
+offset = 0
+for i in range(360):
+    byte_array = wrapper.api_request('games', 'fields *; offset ' + str(offset) + '; where release_dates.y > 1999;')
+    # append byte_array to same json every time we loop and save it to a file
+    data = json.loads(byte_array)
+    json_arr.append(data)
+    with open('full_igdb_data.json', 'a') as outfile:
+        json.dump(json_arr, outfile, indent=4)
+    offset += 500
