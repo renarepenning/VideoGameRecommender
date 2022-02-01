a8cd103f2300b2f2e53bd1c88ebe382c5b769660
kromer-creator <69378589+kromer-creator@users.noreply.github.com>
2022-02-01 14:47:20 -0600

    Updated query params

--- Algorithm_Current/scraping_games.py
+++ Algorithm_Current/scraping_games.py
@@ -26,10 +26,12 @@ wrapper = IGDBWrapper('8kx354hpu7lzs6ga7c1ktafopq30qq', access_token)
 json_arr = []
 offset = 0
 for i in range(360):
-    byte_array = wrapper.api_request('games', 'fields *; offset ' + str(offset) + '; where release_dates.y > 1999;')
+    byte_array = wrapper.api_request('games', 'fields genres, themes, game_modes, tags, platforms, keywords, '
+                                              'involved_companies; offset ' + str(offset) + '; where release_dates.y '
+                                                                                            '> 1999;')
     # append byte_array to same json every time we loop and save it to a file
     data = json.loads(byte_array)
     json_arr.append(data)
-    with open('full_igdb_data.json', 'a') as outfile:
+    with open('all_igdb_data.json', 'a') as outfile:
         json.dump(json_arr, outfile, indent=4)
     offset += 500
