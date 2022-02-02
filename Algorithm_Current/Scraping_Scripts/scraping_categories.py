7ddaf039e84d774b96e1ea2147e88a1606ae52ae
kromer-creator <69378589+kromer-creator@users.noreply.github.com>
2022-02-01 21:06:33 -0600

    Wrote Script to Generate JSON Files For Ids to Keys

---
+++ Algorithm_Current/Scraping_Scripts/scraping_categories.py
@@ -0,0 +1,33 @@
+# Kelly Romer
+
+import requests
+from igdb.wrapper import IGDBWrapper
+import json
+
+# Get Access Token
+def get_token():
+    client_id = '8kx354hpu7lzs6ga7c1ktafopq30qq'
+    client_secret = 'yucgxc2bqiobcn2pfadu5bui5amuan'
+    body = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        "grant_type": 'client_credentials'
+    }
+    r = requests.post('https://id.twitch.tv/oauth2/token', body)
+    keys = r.json()
+    return keys['access_token']
+
+access_token = get_token()
+
+# Create Wrapper
+wrapper = IGDBWrapper('8kx354hpu7lzs6ga7c1ktafopq30qq', access_token)
+
+# JSON API request
+params = ['genres', 'themes', 'game_modes', 'platforms', 'keywords']#tags involved_companies
+for item in params:
+    byte_array = wrapper.api_request(item, 'fields id, name; limit 500;')
+    # append byte_array to same json every time we loop and save it to a file
+    data = json.loads(byte_array)
+    with open(item+'_to_ids.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
