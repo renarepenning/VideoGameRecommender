ae8e17c03103198c9bb5ad74e626ab24f6951335
kromer-creator <69378589+kromer-creator@users.noreply.github.com>
2022-02-01 21:10:35 -0600

    remove extraneous comment

--- Algorithm_Current/Scraping_Scripts/scraping_categories.py
+++ Algorithm_Current/Scraping_Scripts/scraping_categories.py
@@ -26,7 +26,6 @@ wrapper = IGDBWrapper('8kx354hpu7lzs6ga7c1ktafopq30qq', access_token)
 params = ['genres', 'themes', 'game_modes', 'platforms', 'keywords']
 for item in params:
     byte_array = wrapper.api_request(item, 'fields id, name; limit 500;')
-    # append byte_array to same json every time we loop and save it to a file
     data = json.loads(byte_array)
     with open(item+'_to_ids.json', 'w') as outfile:
         json.dump(data, outfile, indent=4)
