e87628527ed2018a3125adee0f93cb8526c4f369
Rena Repenning <renareps108@gmail.com>
2022-02-06 15:35:52 -0600

    add homepage

--- dj-vgr/dj_vgr/urls.py
+++ dj-vgr/dj_vgr/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include#, url
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls), # add urlconf from user
-    
-    path('', views.home, name="home"), # path for home page
+
+    path('', views.home, name="front-page"), # path for home page
 
     path('user/', include('user.urls')), # sends urls with user prefix to user file
 
