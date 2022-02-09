9251fd6b9683a0e665e6e78a002d7e202e59a983
Emily O'Connell <emilykoconnell@gmail.com>
2022-02-09 00:37:40 -0600

    setup test heroku and installed django into venv

--- dj-vgr/user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import myUser, Game, Mood
-
-admin.site.register(myUser) # insert ref to new table
-admin.site.register(Game)
-admin.site.register(Mood)
