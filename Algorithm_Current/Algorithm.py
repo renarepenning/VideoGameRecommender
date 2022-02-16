bf809b3d1219d789898e712ff7a2fd7e1750aebf
mrau1 <53070965+mrau1@users.noreply.github.com>
2022-02-16 13:11:33 -0600

    Update Algorithm.py

--- Algorithm_Current/Algorithm.py
+++ Algorithm_Current/Algorithm.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import pandas as pd
 import numpy as np
 import os
+import threading
+import time
 
-df = pd.read_csv('C:/Users/Matthew Raw/Downloads/IGDB_games.csv').set_index('id')
+df = pd.read_csv('C:/Users/Trader00/Downloads/IGDB_games.csv').set_index('id')
 test = df.iloc[5005]
+
+master_cols = ['genres', 'themes', 'game_modes','tags', 'platforms', 'keywords']
 
 def conjunction(lst1, lst2):
     return list(set(lst1) & set(lst2))
@@ -19,32 +30,135 @@ def clean(array):
     return list(map(lambda x: int(x), array))
 
 def transform_column(target, column, df=df):
+    start = time.time()
     test = clean(target[column])
+    clean_time = time.time()
+    print('Singular Clean Time', clean_time - start)
     col = df[column].dropna().apply(clean)
+    clean_col_time = time.time() - clean_time
+    print('Column Clean Time', clean_col_time)
     func = lambda x: conjunction(x, test)
     conj = col.apply(func)
     func = lambda x: disjunction(x, test)
     disj = col.apply(func)
-    score = conj.apply(lambda x: len(x)) / disj.apply(lambda x: len(x)) 
+    score = conj.apply(lambda x: len(x)) / disj.apply(lambda x: len(x))
+    score_time = time.time() - clean_time - clean_col_time
+    print('Score Time', score_time)
+
     return score
-    
+
+def get_input(game):
+    try:
+        return df[df['name'] == game].iloc[0]
+    except:
+        print('FUCK YOU')
+
+   
 def transform(columns, test, df=df):
+    df = df.set_index("name")
+    df['name'] = df.index
+    
+    master = pd.DataFrame(columns=df.index.tolist(),
+                          index=df.index.tolist())    
+    out_columns = ['name']
     df['Columns Counted'] = 0
+    start = time.time()
     for col in columns:
-        print(col)
         col_name = col + ' score'
+        out_columns.append(col_name)
         df[col_name] = 0
         try:
-            ser = transform_column(test, col)
-            for id in ser.index:
-                try:
-                    df.loc[id, col_name] = ser.loc[id]
-                    df.loc[id, 'Columns Counted'] = df.loc[id, 'Columns Counted'] + 1
-                except:
-                    pass
+            ser = transform_column(test, col, df=df)
+            df[col_name] = ser
+
         except:
             pass
     col_map = list(map(lambda x: x + ' score', columns))
     df['Total'] = df[col_map].sum(axis=1)
-    return df
-transform(['genres', 'themes', 'game_modes','tags', 'platforms', 'keywords'], test).sort_values('Total')
+    end = time.time() - start
+    print('Transform Time', end)
+    return df[out_columns + ['Total']]
+
+def save_file(game, columns:list, df:pd.DataFrame=df):
+    if not os.path.exists("Saver"):
+        os.mkdir('Saver')
+    
+    try:
+        row = get_input(game)
+    except:
+        print(game, 'fucking sucks')
+   
+    if f'{game}.csv' not in os.listdir('Saver'):
+        df= transform(columns, row).set_index('name')
+        df.to_csv(f'Saver/{game}.csv')
+    else:
+        if pd.read_csv(f'Saver/{game}.csv').columns.tolist() == columns:
+            print(game)
+        else:
+            df= transform(columns, row).set_index('name')
+            df.to_csv(f'Saver/{game}.csv')
+            return df
+
+
+def multiple_games(games:list, df=df, columns:list=['genres', 'themes', 'game_modes','tags', 'platforms', 'keywords']):
+    #print(df.name.tolist())
+    SAVE_DIR = 'Saver'
+    """if os.path.exists(SAVE_DIR):
+        for file in os.listdir(SAVE_DIR):
+            os.remove(os.path.join(SAVE_DIR, file))
+        os.rmdir(SAVE_DIR)
+    """
+    if not os.path.exists(SAVE_DIR):
+        print("FUCK")
+        os.mkdir(SAVE_DIR)
+    threads = list()
+   
+    for game in games:
+        print(game)
+        save_file(game, columns, df)
+        """
+        x = threading.Thread(target=save_file, args=(game, columns, df,))
+        threads.append(x)
+        x.start()
+        """
+    #for thread in threads:
+     #   thread.join()
+    for game in games:
+        print(f'Saver/{game}.csv')
+        pd.read_csv(f'Saver/{game}.csv')
+   
+
+
+
+def preprocess(df:pd.DataFrame, columns=master_cols):
+    df = df.set_index("name")
+    df = df[columns]
+    df['name'] = df.index
+    master = pd.DataFrame(columns=df.index.tolist(),
+                          index=df.index.tolist())
+    start = time.time()
+    threads = list()
+    
+    
+    for game in df.index:
+        x = threading.Thread(target=save_file, args=(game, columns))
+        threads.append(x)
+        x.start()
+        print(game)
+        #save_file(game, columns)
+    for thread in threads:
+        thread.join()
+    
+    end = time.time() - start
+    print('Start to Finish', end, 'Seconds')
+    print('Average Time per sample', end/len(df), 'Seconds')
+    
+    
+    
+#out = save_file('Out of the Park Baseball 12', master_cols)
+
+#print(out)
+
+
+preprocess(df.iloc[125:150])
+#print(get_input('Out of the Park Baseball 12'))
