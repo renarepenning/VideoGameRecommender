dedc3d87580bd5dfc8d10700d2679eb7aa600931
Kelly Romer <kromer@tulane.edu>
2022-03-08 18:46:33 -0600

    Update scraping_companies.py

--- Algorithm_Current/Scraping_Scripts/scraping_companies.py
+++ Algorithm_Current/Scraping_Scripts/scraping_companies.py
@@ -1,22 +1,9 @@
 # Kelly Romer
-
-import requests
 from igdb.wrapper import IGDBWrapper
 import json
+from VideoGameRecommender.Algorithm_Current.Scraping_Scripts.token import get_token
 
-# Get Access Token
-def get_token():
-    client_id = '8kx354hpu7lzs6ga7c1ktafopq30qq'
-    client_secret = 'yucgxc2bqiobcn2pfadu5bui5amuan'
-    body = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        "grant_type": 'client_credentials'
-    }
-    r = requests.post('https://id.twitch.tv/oauth2/token', body)
-    keys = r.json()
-    return keys['access_token']
-
+# Create Access Token
 access_token = get_token()
 
 # Create Wrapper
